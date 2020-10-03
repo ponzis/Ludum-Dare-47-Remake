278b813919bd812dea6381a4fce46655e194cd1b
ponzis <ponzis.online@gmail.com>
2020-10-02 20:48:37 -0400

    This is the first commit for the unity game files.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int Health = 3;
+
+    public float Speed = 1f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update.");
+    }
+}
