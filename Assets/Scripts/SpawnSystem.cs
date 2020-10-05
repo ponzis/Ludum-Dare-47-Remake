d8f94e9051b5c81e89b155a666cb19eee97eef5f
ponzis <ponzis.online@gmail.com>
2020-10-05 18:50:18 -0400

    Added WebGL

--- Assets/Scripts/SpawnSystem.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-
-public abstract class SpawnSystem : ScriptableObject
-{
-    public abstract void Spawn(Vector3 position);
-
-    public abstract void DrawGizmos(Vector3 position);
-}
