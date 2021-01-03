33f6381266d69dc8a925ab45253a110380d69ca6
ponzis <ponzis.online@gmail.com>
2021-01-02 23:57:24 -0500

    Added support to show version number on the game based on git commit.

--- Assets/Scripts/CassetteManager.cs
+++ Assets/Scripts/CassetteManager.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
 public class CassetteManager : MonoBehaviour
---
+++ Assets/Scripts/DebugUI.cs
@@ -0,0 +1,40 @@
+﻿using Attributes;
+using UnityEditor;
+using UnityEngine;
+using Utils;
+
+[ExecuteInEditMode]
+public class DebugUI : MonoBehaviour
+{
+    [Label] public string BuildVersion;
+
+    public Rect Rect;
+
+    [Header("Debug")] public bool ShowVersion = true;
+
+    public void Awake()
+    {
+        BuildVersion = UpdateBuildVersion();
+    }
+
+    private string UpdateBuildVersion()
+    {
+        var buildVersion = Git.BuildVersion;
+        return buildVersion;
+    }
+
+    void OnGUI()
+    {
+        if (!ShowVersion) return;
+        GUI.Label(Rect, BuildVersion);
+    }
+
+    void OnDrawGizmos()
+    {
+        if (!ShowVersion) return;
+#if UNITY_EDITOR
+        Handles.color = Color.white;
+        Handles.Label(Vector3.zero, BuildVersion);
+#endif
+    }
+}
